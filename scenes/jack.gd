fc78ed59d6f8ed9c4e9a851c8275beac556b345a
MahBestBro <76799220+MahBestBro@users.noreply.github.com>
2025-01-26 15:13:54 +1100

    Jack Click and Dragging + Cables
    Can click and drag around jacks. Ideally the jacks would smooth to position but can't think of how to do that right now.
    
    Jacks also have cables attached to them. Right now just a straight line, but if we have any spare time we can make them drape a bit.

--- scenes/jack.gd
+++ scenes/jack.gd
@@ -1,20 +1,8 @@
 extends Node2D
 
-@onready var sprite: Sprite2D = $Sprite2D
-
-#Ideally this would be a constant or some predefined number, but for now I think
-#this will work
-func radius() -> float:
-	return sprite.transform.get_scale().x * sprite.texture.get_width()
+@onready var line: Line2D = $Line2D
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print(radius())
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var mouse_pos := get_local_mouse_position()
-	var r := radius()
-	if transform.get_origin().distance_squared_to(mouse_pos) <= r * r:
-		print("Inside")
+	assert(len(line.points) == 2)
+	line.points[1] = to_local(Vector2(40, -100))
