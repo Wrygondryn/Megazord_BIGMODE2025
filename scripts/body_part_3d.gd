02b802526f7c53dc20c91969b295091f2ce67a8f
MahBestBro <653mOker@gmail.com>
2025-02-02 15:34:42 +1100

    Body Part HP Bars
    Body Parts now have HP bars that float nearby them and always face the camera.

--- scripts/body_part_3d.gd
+++ scripts/body_part_3d.gd
@@ -1,16 +1,17 @@
 extends Node3D
 
 @export var kind: Helpers.BodyPart = Helpers.BodyPart.ANY
+@export var hp_colour: Color
 @export_range(10.0, 10000.0, 1.0, "or_greater", "hide_slider") var max_hp: float
 
 @onready var modules: Node = $Modules
 @onready var hp_display_temp: Label3D = $HPDisplay_TEMP
 @onready var condition_display_temp: Label3D = $ConditionDisplay_TEMP
 @onready var condition_timer: Timer = $ConditionTimer
+@onready var hp_bar: Sprite3D = $HPBar
 
 var hp: float
 var condition := Helpers.Condition.NONE 
-
 
 signal action_ready
 
@@ -23,14 +24,27 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if condition_timer.is_stopped():
+		condition = Helpers.Condition.NONE
+	
 	hp_display_temp.text = "HP - %.0f" % hp
 	if condition != Helpers.Condition.NONE:
 		condition_display_temp.text = Helpers.Condition.keys()[condition]
 	else:
 		condition_display_temp.text = ""
 	
-	if condition_timer.is_stopped():
-		condition = Helpers.Condition.NONE
+	#NOTE: By default, Gradient starts with 2 points
+	var hp_gradient := Gradient.new()
+	hp_gradient.interpolation_mode = Gradient.GRADIENT_INTERPOLATE_CONSTANT
+	hp_gradient.set_color(0, hp_colour)
+	hp_gradient.set_offset(1, hp / max_hp)
+	hp_gradient.set_color(1, Color.BLACK)
+	
+	var hp_texture := GradientTexture1D.new()
+	hp_texture.gradient = hp_gradient
+	hp_texture.width = hp_bar.region_rect.size.x
+	
+	hp_bar.texture = hp_texture
 
 func _on_module_fully_charged(data: ModuleData) -> void:
 	match condition:
