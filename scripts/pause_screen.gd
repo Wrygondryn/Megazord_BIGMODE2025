4f87cd6fc000f0094d9199465f5dc33fd4a99cae
MahBestBro <653mOker@gmail.com>
2025-02-03 15:11:44 +1100

    Pause Menu + Controls Update

---
+++ scripts/pause_screen.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("pause_game"):
+		get_tree().paused = !get_tree().paused
+		
+		if get_tree().paused: 
+			show()
+		else:
+			hide()
